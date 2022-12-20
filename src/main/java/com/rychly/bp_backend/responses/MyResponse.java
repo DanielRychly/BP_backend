28d6e18401c84e701f9d8674e51c7bd10009e0a3
xrychly <xrychly@stuba.sk>
2022-12-20 20:30:50 +0100

    did some more sonar lint(ing)

--- src/main/java/com/rychly/bp_backend/responses/MyResponse.java
+++ src/main/java/com/rychly/bp_backend/responses/MyResponse.java
@@ -5,9 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // json ignore -> https://stackoverflow.com/questions/45915851/could-not-write-json-no-serializer-found-for-class-org-json-jsonobject-and-no-p
+
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class MyResponse {
     private String response;
